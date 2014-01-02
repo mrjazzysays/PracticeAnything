b437645478ebd9437b2ec5472e5b460e6e1255f0
unknown <jcedilla@clinical-jeff-PC.pathway.local>
2014-01-02 11:50:26 -0800

    successful2

---
+++ src/com/example/practiceanything/WeekActivity.java
@@ -0,0 +1,32 @@
+package com.example.practiceanything;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class WeekActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_week);
+		
+		TextView tv = (TextView)findViewById(R.id.listWeek);
+		
+		Intent intent = getIntent();
+		String listweek = intent.getExtras().getString("listweek");
+		tv.setText(listweek);
+		
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.week, menu);
+		return true;
+	}
+
+}
