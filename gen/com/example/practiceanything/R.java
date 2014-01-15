f593e03daeac61a7d6e5cf97b64d159e5976e452
unknown <jcedilla@clinical-jeff-PC.pathway.local>
2014-01-14 16:54:18 -0800

    Added linearlayout to activity_week

--- gen/com/example/practiceanything/R.java
+++ gen/com/example/practiceanything/R.java
@@ -24,23 +24,31 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
+        public static final int action_settings=0x7f080012;
         public static final int addUser=0x7f080009;
+        public static final int button1=0x7f08000e;
+        public static final int button2=0x7f080011;
+        public static final int button3=0x7f08000c;
+        public static final int button4=0x7f08000b;
+        public static final int button5=0x7f08000d;
+        public static final int button6=0x7f08000f;
+        public static final int button7=0x7f080010;
         public static final int calendarView1=0x7f080000;
         public static final int currentDay=0x7f080003;
-        public static final int deleteUsername=0x7f080006;
+        public static final int deleteUsername=0x7f08000a;
         public static final int displayDate=0x7f080001;
-        public static final int editText1=0x7f080007;
+        public static final int editText1=0x7f080004;
         public static final int editText2=0x7f080005;
         public static final int goToWeek=0x7f080002;
-        public static final int listView1=0x7f080008;
-        public static final int listWeek=0x7f080004;
-        public static final int users=0x7f08000a;
+        public static final int listView1=0x7f080006;
+        public static final int listWeek=0x7f080007;
+        public static final int users=0x7f080008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_week=0x7f030001;
         public static final int simple_list_item=0x7f030002;
+        public static final int testing=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
